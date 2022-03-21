acda15dcf09d9e6f2ac71db588c1c5a92d60b594
oumaima <oumaimaabouelhaitam@gmail.com>
2022-03-21 09:26:28 +0100

    uodate

--- Mini_Calculatrice/Function.js
+++ Mini_Calculatrice/Function.js
@@ -12,42 +12,29 @@ function calculer()
 } 
 function afficher(num) 
 { 
-
     var myString =  output.value
     var lastChar = myString[myString.length - 1]
-    if (myString.length < 18) {
-        if(num=='.' && !canIAddDot()  ){
-       
-            return;
-    
+    if (myString.length <= 18) {
+        if(num=='.' && !canIAddDot()){
+        return;   
     }
-    
         else
         if (!isNaN(lastChar) || lastChar == null || !isNaN(num)) {
            output.value = myString + num 
         }else{
             output.value = myString.replace(lastChar, num)
-        }
-            
-        
-           
-        
-       
+        }   
     }
 } 
-
-   
- 
-
 function del() 
 {  
     var exp = output.value
     output.value = exp.substring(0,exp.length - 1);
  }
 
-function canIAddDot(){
+function canIAddDot()
+{
     const regex=/(^\d+$)|(^(\d+\.{0,1}\d*)([*\-+\/]?(\d+\.?\d*))*[*\-+\/]\d+$)/gm;
-   
     return output.value.match(regex);
 }
 
